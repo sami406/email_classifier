25b6fef609a18507fb82924c11e3b6bbcb02dd60
SAHIL PRAJAPATI <sahilprajapati@SAHILs-MacBook-Air.local>
2025-04-22 13:37:13 +0530

    Add application file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,22 @@
+#FROM python:3.9
+#WORKDIR /app
+#COPY . /app
+#RUN pip install --no-cache-dir -r requirements.txt
+#EXPOSE 7860
+#CMD ["python", "app.py"]
+# Read the doc: https://huggingface.co/docs/hub/spaces-sdks-docker
+# you will also find guides on how best to write your Dockerfile
+
 FROM python:3.9
+
+RUN useradd -m -u 1000 user
+USER user
+ENV PATH="/home/user/.local/bin:$PATH"
+
 WORKDIR /app
-COPY . /app
-RUN pip install --no-cache-dir -r requirements.txt
-EXPOSE 7860
-CMD ["python", "app.py"]
+
+COPY --chown=user ./requirements.txt requirements.txt
+RUN pip install --no-cache-dir --upgrade -r requirements.txt
+
+COPY --chown=user . /app
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "7860"]
--- app.py
+++ app.py
@@ -2,27 +2,34 @@ from flask import Flask, request, jsonify
 from model import load_model, predict_category
 from masking import mask_pii, unmask_pii
 
-app = Flask(__name__)
-model, vectorizer = load_model()
+#app = Flask(__name__)
+#model, vectorizer = load_model()
 
 
-@app.route("/classify", methods=["POST"])
-def classify():
-    data = request.get_json()
-    email = data.get("email", "")
+#@app.route("/classify", methods=["POST"])
+#def classify():
+  #  data = request.get_json()
+   # email = data.get("email", "")
 
-    masked_email, pii_data = mask_pii(email)
-    category = predict_category(masked_email, model, vectorizer)
-    restored_email = unmask_pii(masked_email, pii_data)
+   # masked_email, pii_data = mask_pii(email)
+  #  category = predict_category(masked_email, model, vectorizer)
+  #  restored_email = unmask_pii(masked_email, pii_data)
 
-    return jsonify({
-        "category": category,
-        "email": restored_email
-    })
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=7860)
+  #  return jsonify({
+       # "category": category,
+      #  "email": restored_email
+    #})
 
 
+#if __name__ == "__main__":
+#    app.run(host='0.0.0.0', port=7860)
 
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def greet_json():
+    return {"Hello": "World!"}
